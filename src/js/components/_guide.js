96d7b2c2a604ce08a5c01bab244762c99234b4a4
whats0n <mrdvv@mail.ru>
2018-01-31 22:37:33 +0200

    update success screen

--- src/js/components/_guide.js
+++ src/js/components/_guide.js
@@ -6,13 +6,19 @@ import {ACTIVE, PREV, OPEN} from '../_constants';
 	const directionPrev = 'prev';
 	const directionNext = 'next';
 
+	const duration = 300;
+	const delay = 2000;
+
 	$('[data-guide]').each((i, guide) => {
 		guide = $(guide);
 		//buttons
 		const controls = guide.find('[data-guide-step]');
 		const buttons = guide.find('[data-guide-direction]');
+		const submit = $('[data-guide-submit]');
 		//items
 		const container = guide.find('[data-guide-container]');
+		const success = $('[data-success]');
+		const loader = $('[data-loader]');
 		//prgressbar
 		const line = guide.find('[data-guide-line]');
 		const offset = 100/(controls.length - 1);
@@ -48,25 +54,28 @@ import {ACTIVE, PREV, OPEN} from '../_constants';
 			swipe: false
 		});
 
-		container.on('beforeChange', (e, slick, prev, next) => {
-			const control = controls.filter(`[data-guide-step="${next}"]`);
-			const allPrevControls = control.prevAll('[data-guide-step]');
-			const allNextControls = control.nextAll('[data-guide-step]');
-			
-			allNextControls
-				.removeClass(PREV)
-				.removeClass(ACTIVE);
-			control
-				.removeClass(PREV)
-				.addClass(ACTIVE);
-			allPrevControls
-				.removeClass(ACTIVE)
-				.addClass(PREV);
+		container
+			.on('beforeChange', (e, slick, prev, next) => {
+				const control = controls.filter(`[data-guide-step="${next}"]`);
+				const allPrevControls = control.prevAll('[data-guide-step]');
+				const allNextControls = control.nextAll('[data-guide-step]');
+				
+				allNextControls
+					.removeClass(PREV)
+					.removeClass(ACTIVE);
+				control
+					.removeClass(PREV)
+					.addClass(ACTIVE);
+				allPrevControls
+					.removeClass(ACTIVE)
+					.addClass(PREV);
 
-			const linePositionX = offset*next - 100;
-			line.css('transform', `translate3d(${linePositionX}%,0,0)`);
-
-		});
+				const linePositionX = offset*next - 100;
+				line.css('transform', `translate3d(${linePositionX}%,0,0)`);
+			})
+			.on('afterChange', (e, slick, current) => {
+				success.hasClass(OPEN) && success.fadeOut(duration, () => success.removeClass(OPEN));
+			});
 
 		buttons.each((i, button) => {
 			button = $(button);
@@ -85,6 +94,18 @@ import {ACTIVE, PREV, OPEN} from '../_constants';
 			});
 		});
 
+		submit.on('click', e => {
+			e.preventDefault();
+			loader.fadeIn(duration);
+
+			setTimeout(function() {
+				loader.fadeOut(duration);
+				success.fadeIn(duration, function() {
+					success.addClass(OPEN);
+				});
+			}, delay);
+		});
+
 		// controls.each((i, control) => {
 		// 	control = $(control);
 		// 	const step = control.data('guide-step');
@@ -95,5 +116,6 @@ import {ACTIVE, PREV, OPEN} from '../_constants';
 		// 	});
 		// });
 	});
+
 
 })();
